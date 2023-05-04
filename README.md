a87c9a35c952d54a4434e0e2a39ae8cdb593bd9c
vremfort94 <vremfort@gmail.com>
2023-05-04 13:23:15 +0530

    twitter

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Twitter-clone
+# daisy-TwitterClone
---
+++ posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+def index(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.errors.as_json())
+    posts = Post.objects.all().order_by('-created_at')[:20]
+    return render(request, "posts.html", {'posts': posts})
+def delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+def edit(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.errors.as_json())
+    return render(request, "edit.html",{"post":post})
+    
+
+def LikeView(request, post_id):
+    post = Post.objects.get(id= post_id)
+    new_value= post.likes+1
+    post.likes = new_value
+    post.save()
+    return HttpResponseRedirect('/')
